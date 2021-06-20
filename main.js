dfd3e12dba3e3b6cb653065a0a416e3febf3b490
internetcharles <charlesodellsmith@gmail.com>
2021-06-20 15:10:30 -0700

    animations and projects

--- main.js
+++ main.js
@@ -2,17 +2,44 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import * as dat from 'dat.gui'
+import * as dat from 'dat.gui';
+import * as TWEEN from '@tweenjs/tween.js';
+import { gsap } from 'gsap';
 
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+
+// DOM Nodes
+// const aboutDiv = document.createElement('div');
+// const aboutContent = document.createTextNode("ABOUT");
+// const projectsContent = document.createTextNode("PROJECTS");
+// const creditsContent = document.createTextNode("CREDITS");
+
+// aboutDiv.className = "about";
+// aboutContent.className = "about-text";
+// projectsContent.className = "projects-text";
+// creditsContent.className = "credits-text";
+
+// aboutDiv.appendChild(aboutContent);
+// aboutDiv.appendChild(projectsContent);
+// aboutDiv.appendChild(creditsContent);
+
+// const currentDiv = document.getElementById('info');
+// document.body.insertBefore(aboutDiv, currentDiv)
+
 
 // Canvas
 const canvas = document.querySelector('#bg')
+
+// Conditionals
+let viewingProjects = false;
+let viewingAbout = false;
+let viewingHome = true;
+
+
 
 // Scene
 const scene = new THREE.Scene()
@@ -65,7 +92,6 @@ fontLoader.load(
       text2.rotateY(0.26)
       text2.rotateX(-0.3)
       scene.add(text, text2)
-      text.on('click', () => console.log('hello'))
   }
 )
 
@@ -76,6 +102,9 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
 
 const gltfLoader = new GLTFLoader();
 
@@ -105,9 +134,23 @@ gltfLoader.load(
   }
 )
 
+gltfLoader.load(
+  '/static/projects.glb',
+  (gltf) => {
+  gltf.scene.traverse(function(node) {
+    if (node instanceof THREE.Mesh) {
+      node.castShadow = true;
+    }
+  });
+  gltf.scene.position.set(-10, 2, -10)
+  scene.add(gltf.scene);
+  }
+);
+
 
 window.addEventListener('load', () =>
 {
+
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -147,16 +190,17 @@ window.addEventListener('resize', () =>
     // camera.position.y = (sizes.width / sizes.height) * 8;
     // camera.position.z = (sizes.width / sizes.height) * 3;
 
-    if(sizes.width < 1200) {
-      camera.lookAt(7.5, 0, -10.5)
-      camera.position.set(11.5, 8, 8)
-    } else {
-      camera.lookAt(0, -1, -8)
-      camera.position.x = (sizes.width / sizes.height) * 15.5;
-      camera.position.y = (sizes.width / sizes.height) * 8;
-      camera.position.z = (sizes.width / sizes.height) * 3;
+    if (!viewingProjects) {
+      if(sizes.width < 1200) {
+        camera.lookAt(7.5, 0, -10.5)
+        camera.position.set(11.5, 8, 8)
+      } else {
+        camera.lookAt(0, -1, -8)
+        camera.position.x = (sizes.width / sizes.height) * 15.5;
+        camera.position.y = (sizes.width / sizes.height) * 8;
+        camera.position.z = (sizes.width / sizes.height) * 3;
+      }
     }
-
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -185,9 +229,48 @@ rectLight3.position.set( -2.6, 2.85, -2.6 );
 rectLight3.rotateY(180)
 scene.add( rectLight3 );
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight)
 
+const tweenCamera = (camera, position, duration, target) => {   
+  new TWEEN.Tween(camera.position).to({
+    x: position[0],
+    y: position[1],
+    z: position[2]
+  }, duration)
+  .easing(TWEEN.Easing.Quadratic.InOut)
+  .start();
+}
+
+const tweenToProjects = () => {
+  // backup original rotation
+  var startRotation = camera.quaternion.clone();
+
+  // final rotation (with lookAt)
+  camera.lookAt( -6, 2, -10 );
+  var endRotation = camera.quaternion.clone();
+
+  // revert to original rotation
+  camera.quaternion.copy( startRotation );
+
+  // Tween
+  new TWEEN.Tween( camera.quaternion ).to( endRotation, 600 ).start();
+}
+
+const tweenToAbout = () => {
+  // backup original rotation
+  var startRotation = camera.quaternion.clone();
+
+  // final rotation (with lookAt)
+  camera.lookAt( -0.95, 2.3, -1 );
+  var endRotation = camera.quaternion.clone();
+
+  // revert to original rotation
+  camera.quaternion.copy( startRotation );
+
+  // Tween
+  new TWEEN.Tween( camera.quaternion ).to( endRotation, 600 ).start();
+}
 
 /**
  * Renderer
@@ -215,14 +298,46 @@ const tick = () =>
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
 
-    camera.position.x = 12 + Math.sin(elapsedTime) * 0.1
-    camera.position.y = 8 + Math.sin(elapsedTime) * 0.1
-
+    if(viewingHome) {
+      camera.position.x = 12 + Math.sin(elapsedTime) * 0.1
+      camera.position.y = 8 + Math.sin(elapsedTime) * 0.1
+    }
+    TWEEN.update();
     // Render
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
+
+const projectsElement = document.getElementById('projects')
+
+document.querySelector('.projects-text').addEventListener("click", () => {
+  TWEEN.removeAll();
+  projectsElement.classList.remove('fadeout');
+  viewingProjects = true;
+  viewingAbout = false;
+  viewingHome = false;
+  tweenCamera(camera, [-5, 2, -10], 5000 );
+  setTimeout(() => {
+    tweenToProjects()
+  }, 5000)
+  setTimeout(() => {
+    projectsElement.classList.add('fadein');
+  }, 6000)
+});
+
+document.querySelector('.about-text').addEventListener("click", () => {
+  TWEEN.removeAll();
+  projectsElement.classList.remove('fadein');
+  projectsElement.classList.add('fadeout');
+  viewingProjects = false;
+  viewingAbout = true;
+  viewingHome = false;
+  tweenCamera(camera, [4, 5, 1], 5000 );
+  setTimeout(() => {
+    tweenToAbout()
+  }, 5000)
+});
 
 tick()
